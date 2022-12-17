bb3ed4979995394c34a882a00502be2ee1c2a6d6
Jon Pesta <38979691+codingpierogi@users.noreply.github.com>
2022-12-17 15:28:17 +0000

    Checkpoint for verbose output.

--- cmd/root.go
+++ cmd/root.go
@@ -8,16 +8,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var Verbose bool
+
 var rootCmd = &cobra.Command{
-	Use: "gurl",
+	Use:   "gurl",
+	Short: "Golang curl",
+	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		body, err := fetch.Body(args[0])
+		_, err := fetch.Body(args[0])
 
 		if err != nil {
 			fmt.Printf("Error making request")
 		}
 
-		fmt.Print(body)
+		//fmt.Print(body)
 	},
 }
 
@@ -29,5 +33,5 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "Make the operation more talkative")
 }
--- pkg/fetch.go
+++ pkg/fetch.go
@@ -1,12 +1,34 @@
 package fetch
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 )
 
 func Body(url string) (string, error) {
 	res, err := http.Get(url)
+
+	// > GET / HTTP/2
+	// > Host : www.google.com
+	// > user-agent: curl/7.68.0
+	// > accept: */*
+	// >
+	// *
+	// < HTTP/2 200
+	// < date: Sat, 17 Dec 2022 14:41:44 GMT
+	// < expires: -1
+	// < cache-control: private, max-age=0
+	// < content-type: text/html; charset=ISO-8859-1
+	// <
+	fmt.Println(res.Request.URL.Opaque)
+	// for n, v := range res.Request{
+	// 	fmt.Println(n, v)
+	// }
+	fmt.Printf("> GET %s \n", res.Request.Proto)
+	fmt.Printf("> Host: %s \n", res.Request.Host)
+	fmt.Printf("> user-agent: %s \n", res.Request.UserAgent())
+	fmt.Printf("> accept: %s \n", res.Request.Header["Accept"])
 
 	if err != nil {
 		return "", err
