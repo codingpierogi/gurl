7fc733aac77eae7a1e49859ebf3a47ccc583544d
Jon Pesta <38979691+codingpierogi@users.noreply.github.com>
2023-01-06 11:12:35 +0000

    Lowercased non-exported vars.

--- cmd/root.go
+++ cmd/root.go
@@ -8,9 +8,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Output []string
-var Verbose bool
-var UserAgent string
+var output []string
+var verbose bool
+var userAgent string
 
 var rootCmd = &cobra.Command{
 	Use:   "gurl",
@@ -18,10 +18,10 @@ var rootCmd = &cobra.Command{
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		options := fetch.Options{
-			Verbose:   Verbose,
-			UserAgent: UserAgent,
+			Verbose:   verbose,
+			UserAgent: userAgent,
 		}
-		curl.Run(args, Output, options)
+		curl.Run(args, output, options)
 	},
 }
 
@@ -33,7 +33,7 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.PersistentFlags().StringSliceVarP(&Output, "output", "o", []string{""}, "Write to file instead of stdout")
-	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "Make the operation more talkative")
-	rootCmd.PersistentFlags().StringVarP(&UserAgent, "user-agent", "A", "curl/7.68.0", "Send User-Agent <name> to server")
+	rootCmd.PersistentFlags().StringSliceVarP(&output, "output", "o", []string{""}, "Write to file instead of stdout")
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Make the operation more talkative")
+	rootCmd.PersistentFlags().StringVarP(&userAgent, "user-agent", "A", "curl/7.68.0", "Send User-Agent <name> to server")
 }
