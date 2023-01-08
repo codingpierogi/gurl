dfb1b89e88890228b540c67c00cb2a6e6fcb5472
Jon Pesta <38979691+codingpierogi@users.noreply.github.com>
2023-01-08 12:42:24 +0000

    Finalized viper external config capability.

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,21 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/codingpierogi/gurl/pkg/curl"
 	"github.com/codingpierogi/gurl/pkg/fetch"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-var output []string
-var verbose bool
-var userAgent string
+var (
+	cfgFile   string
+	output    []string
+	verbose   bool
+	userAgent string
+)
 
 var rootCmd = &cobra.Command{
 	Use:   "gurl",
@@ -33,7 +38,41 @@ func Execute() {
 }
 
 func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "K", "", "Read config from a file")
 	rootCmd.PersistentFlags().StringSliceVarP(&output, "output", "o", []string{""}, "Write to file instead of stdout")
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Make the operation more talkative")
 	rootCmd.PersistentFlags().StringVarP(&userAgent, "user-agent", "A", "curl/7.68.0", "Send User-Agent <name> to server")
+	viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup("verbose"))
+	viper.BindPFlag("user-agent", rootCmd.PersistentFlags().Lookup("user-agent"))
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			os.Exit(1)
+		}
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".gurlrc")
+	}
+
+	viper.SetConfigType("env")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	if !rootCmd.PersistentFlags().Lookup("verbose").Changed && viper.IsSet("verbose") {
+		rootCmd.PersistentFlags().Set("verbose", viper.GetString("verbose"))
+	}
+
+	if !rootCmd.PersistentFlags().Lookup("user-agent").Changed && viper.IsSet("user_agent") {
+		rootCmd.PersistentFlags().Set("user-agent", viper.GetString("user_agent"))
+	}
 }
